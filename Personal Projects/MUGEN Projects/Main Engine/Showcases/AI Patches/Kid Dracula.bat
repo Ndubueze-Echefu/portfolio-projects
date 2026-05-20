ea1e3fa402eaf5613ae0f4829e10359f1d943421
Ndubueze Echefu <endyechefu2nd@gmail.com>
2026-05-19 23:01:20 -0500

    Add log and batch; set game resolution 1600x900
    Add Ikemen.log capturing an overrideCharData-before-load error, add a Kid Dracula .bat to run before/after AI patch matches with AI and color params, and update save/config.json to change GameWidth/GameHeight from 640x480 to 1600x900 to increase game resolution.

---
+++ Personal Projects/MUGEN Projects/Main Engine/Showcases/AI Patches/Kid Dracula.bat
@@ -0,0 +1,3 @@
+cd ../..
+Run_on_Windows.exe -p1 "AI Patches/Kid Dracula/KidDracula (before AI Patch)/KidDracula.def" -p1.ai 8 -p2.color 1 -p2 "AI Patches/Kid Dracula/KidDracula (after AI Patch)/KidDracula.def" -p2.ai 8 -p2.color 2
+pause
